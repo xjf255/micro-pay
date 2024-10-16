e1f7fa41ad68cd4d6f3be15298bd60ee596b624d
xjf255 <juanfher.255@gmail.com>
2024-10-16 13:24:56 -0600

    add CORS

--- src/main/java/com/project/micro_pay/MicroPayApplication.java
+++ src/main/java/com/project/micro_pay/MicroPayApplication.java
@@ -2,6 +2,9 @@ package com.project.micro_pay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MicroPayApplication {
@@ -10,4 +13,16 @@ public class MicroPayApplication {
 		SpringApplication.run(MicroPayApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer conrsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// /** => permite CORS en todas las rutas de la aplicacion
+				registry.addMapping("/**").allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*");
+			}
+		};
+	}
 }
--- src/main/java/com/project/micro_pay/controller/StatusController.java
+++ src/main/java/com/project/micro_pay/controller/StatusController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import com.project.micro_pay.service.StatusService;
 
 @RestController
 @RequestMapping("/api/status")
-@CrossOrigin(origins = "*")
 public class StatusController {
 
   @Autowired
