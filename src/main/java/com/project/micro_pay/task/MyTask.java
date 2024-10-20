3a8a6e336ac425ef0ac13e355a8473a2b2c4e129
xjf255 <juanfher.255@gmail.com>
2024-10-19 20:31:28 -0600

    add task

---
+++ src/main/java/com/project/micro_pay/task/MyTask.java
@@ -0,0 +1,24 @@
+package com.project.micro_pay.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+
+@Component
+public class MyTask {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Scheduled(cron = "0 0 0 * * ?")  
+  public void updateStateCoupon(){
+    LocalDate day = LocalDate.now(); 
+    final Long idStatusExpiration = 6L;
+    final Long idStatusAvailable = 4L;
+    String query = "update coupon set id_status = ? where coupon.expiration_date <= ? & coupon.id_status = ?";
+    int rowAffected = jdbcTemplate.update(query, idStatusExpiration,day, idStatusAvailable);
+    if(rowAffected != 0 ) System.out.println("Coupones actualizados " + rowAffected);
+  }
+}
